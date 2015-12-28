5837ce50e21c763ce4bdec88770f8351d8f6f556
Harley Holt <harley.holt@csats.com>
2015-12-28 15:34:50 -0800

    Update for ecmascript

--- package.js
+++ package.js
@@ -1,26 +1,28 @@
+'use strict';
+
 Package.describe({
-  "name": "arsnebula:appupdate",
-  "summary": "Control Meteor Hotcode push and gracefully notify the user of an update.",
-  "version": "1.0.6",
-  "git": "https://github.com/arsnebula/meteor-app-update.git"
+  'name': 'arsnebula:appupdate',
+  'summary': 'Control Meteor Hotcode push and gracefully notify the user of an update.',
+  'version': '1.0.6',
+  'git': 'https://github.com/arsnebula/meteor-app-update.git'
 });
 
 Package.onUse(function(api) {
 
-  api.versionsFrom("METEOR@1.0");
-  api.use("meteor-platform");
-  api.use("reactive-var");
-  api.use("arsnebula:classx@2.0.5");
+  api.versionsFrom('METEOR@1.0');
+  api.use('meteor-platform');
+  api.use('reactive-var');
+  api.use('ecmascript');
 
   api.addFiles(
     [
-      "lib/js/appupdate.namespace.js",
-      "lib/js/classes/reloadcontroller.js",
-      "lib/templates/newupdatealert/newupdatealert.html",
-      "lib/templates/newupdatealert/newupdatealert.css",
-      "lib/templates/newupdatealert/newupdatealert.js",
-    ], ["client"])
+      'lib/js/appupdate.namespace.js',
+      'lib/js/classes/reloadcontroller.js',
+      'lib/templates/newupdatealert/newupdatealert.html',
+      'lib/templates/newupdatealert/newupdatealert.css',
+      'lib/templates/newupdatealert/newupdatealert.js',
+    ], ['client']);
 
-  api.export("AppUpdate");
+  api.export('AppUpdate');
 
 });
